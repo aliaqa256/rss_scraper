05f4238b5fb88564218df94f739e53b94103a41d
aliaqa@newwin10 <aliaqa@newwin10.com>
2022-09-28 12:20:14 +0330

    un follow feed

--- backend/feeds/urls.py
+++ backend/feeds/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from feeds.views.feeds import FeedsList,ItemsList
-from feeds.views.users import FollowFeed
+from feeds.views.users import FollowFeed,UnFollowFeed
 
 urlpatterns = [
     path('feeds/', FeedsList.as_view(), name='feedslist'),
     path('items/', ItemsList.as_view(), name='itemslist'),
     path('follow-feed/', FollowFeed.as_view(), name='followfeed'),
+    path('unfollow-feed/', UnFollowFeed.as_view(), name='unfollowfeed'),
 ]
--- backend/feeds/views/users.py
+++ backend/feeds/views/users.py
@@ -29,3 +29,25 @@ class FollowFeed(APIView):
         except Exception as e:
             msg = f"Error: {e}"
             return Response(msg, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class UnFollowFeed(APIView):
+    """get feed_id and user(by header bearer) then remove user from feed followers"""
+    permission_classes = [permissions.IsAuthenticated]
+    def post(self, request):
+        try:
+            feed_id= request.data['feed_id']
+            feed = Feed.objects.get(pk=feed_id)
+            user = request.user
+            # if user is not in followers list return error
+            if not Feed.objects.filter(pk=feed_id,followers=user).exists():
+                return Response({'message':"you are not following this feed !" },status=status.HTTP_400_BAD_REQUEST)
+            feed.followers.remove(user)
+            return Response({'message': 'you unfollowed this feed succsessfuly '}, status=status.HTTP_200_OK)
+        except Feed.DoesNotExist:
+            return Response({'message':"feed not found !" },status=status.HTTP_404_NOT_FOUND)
+        except KeyError as e:
+            msg = f"Missing key: {e}"
+            return Response(msg, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            msg = f"Error: {e}"
+            return Response(msg, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
