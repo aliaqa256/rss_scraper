9f4efc40825b123d6d98cb06f230b3b6d36c8595
aliaqa@newwin10 <aliaqa@newwin10.com>
2022-09-28 18:28:07 +0330

    befor swagger

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from . import views
 
+
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login_view'),
     path('refresh/', TokenRefreshView.as_view(), name='new_refresh_token'),
--- backend/config/settings/development.py
+++ backend/config/settings/development.py
@@ -93,10 +93,6 @@ TEMPLATES = [
 
 REST_FRAMEWORK = {
 
-    'DEFAULT_PAGINATION_CLASS': (
-    'rest_framework.pagination.PageNumberPagination',
-    ),
-
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ),
